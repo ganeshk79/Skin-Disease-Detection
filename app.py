41c3458655c4727ea2592dda68a2ba05a0013a5f
ganeshk79 <138688125+ganeshk79@users.noreply.github.com>
2025-04-08 10:21:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def predict():
         return jsonify({'predicted_class': predicted_label})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=4000)
 
