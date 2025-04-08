554ee9cd0c67e8ecc7833507cd6ae6d81a844957
ganeshk79 <138688125+ganeshk79@users.noreply.github.com>
2025-04-08 17:48:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,5 +68,4 @@ def predict():
 
 if __name__ == '__main__':
     # Run with Waitress for production
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(debug=True,host="0.0.0.0",port=4000)
