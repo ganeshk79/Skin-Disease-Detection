5a29cea337dd8a4359a0d77408328499f0483d2d
ganeshk79 <138688125+ganeshk79@users.noreply.github.com>
2025-04-08 22:05:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,6 +65,9 @@ def predict():
         #os.remove(img_path)
 
         return jsonify({'predicted_class': predicted_label})
-	    
+
+
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
