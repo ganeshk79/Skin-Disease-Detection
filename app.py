9becd13701ebae464a6cd684a91c35fb0139301a
ganeshk79 <138688125+ganeshk79@users.noreply.github.com>
2025-04-11 15:05:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,33 +8,36 @@ import os
 import werkzeug
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
-
+UPLOAD_FOLDER = 'uploadimage'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 # Load the trained model
 model = load_model(os.path.join(os.path.dirname(__file__), "sd_model.keras"))
 # Define the classes (adjust according to your model's output)
 # Should match your notebook's classes
 #class_names = ['akiec', 'bcc', 'bkl', 'df', 'mel', 'nv', 'vasc']
-class_names=['actinic keratosis', 'basal cell carcinoma','pigmented benign keratosis', 'dermatofibroma', 'melanoma', 'nevus', 'vascular lesion']
+class_names=['actinic keratosis', 'basal cell carcinoma','pigmented benign keratosis', 'dermatofibroma', 'melanoma', 'melanocytic nevi', 'vascular lesion']
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
 
-    file = request.files['file']
-    if file.filename == '':
+    imagefile = request.files['file']
+    if imagefile.filename == '':
         return jsonify({'error': 'No selected file'})
 
     if (request.method =="POST"):    
         # Save the uploaded image to a temporary location
         # img_path = os.path.join('uploads', file.filename)
         # file.save(img_path)
-        imagefile=request.files['file']
         filename =werkzeug.utils.secure_filename(imagefile.filename)
-        imagefile.save("./uploadimage/"+filename)
-        img_path="./uploadimage/"+filename
+        imagefile.save(filename)
+        img_path=filename
 
         # Preprocess the image img = image.load_img(img_path, target_size=(192, 256), color_mode="rgb")
         # Change this line in your Flask app:
@@ -53,7 +56,10 @@ def predict():
 
 
         # Clean up the uploaded image
-        #os.remove(img_path)
+        try:
+            os.remove(filepath)
+        except Exception as e:
+            print(f"Error removing file: {str(e)}")
 
         return jsonify({'predicted_class': predicted_label})
 
